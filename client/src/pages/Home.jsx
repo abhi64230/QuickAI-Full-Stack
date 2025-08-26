1bff78bc2c94863bb2c42957776b7a568c6161c4
Abhishek <abhisekpradhan615@gmail.com>
2025-08-26 19:12:25 +0530

    Changes 2

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
 import Testimonial from '../components/Testimonial'
 import Plan from '../components/Plan'
 import Footer from '../components/Footer'
-import Office from '../components/office'
+import Office from '../components/Office'
 
 const Home = () => {
   return (
